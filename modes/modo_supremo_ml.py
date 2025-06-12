bba8867efade67509f4c9e71eba5e494ad83ca46
rochaedu1722 <rochaedu@icloud.com>
2025-06-12 19:17:15 -0300

    Delete modes directory

--- modes/modo_supremo_ml.py
+++
@@ -1,8 +0,0 @@
-from core.modelo import gerar_sinais
-from core.telegram_sender import enviar_telegram
-
-def executar():
-    sinais = gerar_sinais()
-    for sinal in sinais:
-        mensagem = f"📢 NOVO SINAL\n{str(sinal)}"
-        enviar_telegram(mensagem)
