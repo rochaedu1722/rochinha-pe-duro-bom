b5d774d00085968574cc220875cdf8e041ae8b04
rochaedu1722 <rochaedu@icloud.com>
2025-06-13 12:51:02 -0300

    Update telegram_sender.py

--- core/telegram_sender.py
+++ core/telegram_sender.py
@@ -6,4 +6,13 @@ CHAT_ID = "1146864652"
 def enviar_telegram(mensagem):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
     data = {'chat_id': CHAT_ID, 'text': mensagem}
-    requests.post(url, data=data)
+
+    try:
+        response = requests.post(url, data=data)
+        if response.status_code == 200:
+            print("✅ Mensagem enviada com sucesso ao Telegram.")
+        else:
+            print(f"❌ Erro ao enviar mensagem: {response.status_code}")
+            print(f"🧾 Detalhes: {response.text}")
+    except Exception as e:
+        print(f"❌ Erro inesperado ao enviar mensagem: {e}")
