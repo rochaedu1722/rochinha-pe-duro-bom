d4bca2919474b79a53665f1c02c874e558b47f18
rochaedu1722 <rochaedu@icloud.com>
2025-06-13 20:55:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-# main.py
+print("✅ Iniciando main.py")
+
 from run_scheduler import executar_varredura
 
+print("🧪 Importação concluída com sucesso.")
+
 if __name__ == "__main__":
+    print("▶️ Chamando executar_varredura()...")
     executar_varredura()
