27774ea01d3960eb78be3e8810f06c9f74f27203
rochaedu1722 <rochaedu@icloud.com>
2025-06-14 16:06:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-print("🟢 MAIN.PY: entrou no arquivo")
+print("✅ TESTE DE VIDA: este print veio direto do main.py")
 
-try:
-    print("🟢 MAIN.PY: tentando importar run_scheduler...")
-    from run_scheduler import executar_varredura
-    print("🟢 MAIN.PY: importação bem-sucedida")
-
-    if __name__ == "__main__":
-        print("🟢 MAIN.PY: __name__ é '__main__' ✅")
-        executar_varredura()
-
-except Exception as e:
-    print("❌ MAIN.PY: erro detectado")
-    print(e)
-    print("🏁 MAIN.PY: finalizado por erro")
+while True:
+    pass
