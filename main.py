a07c3fc0d7e0a6437b802f1e2c378c7bb8b3e49c
rochaedu1722 <rochaedu@icloud.com>
2025-06-13 21:09:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ print("✅ Iniciando main.py")
 
 try:
     from run_scheduler import executar_varredura
-    print("🧪 Importação concluída com sucesso.")
+    print("🧪 Importação do run_scheduler concluída.")
 
     if __name__ == "__main__":
         print("▶️ Chamando executar_varredura()...")
         executar_varredura()
 
 except Exception as e:
-    print("❌ Erro durante a execução do main.py:")
+    print("❌ Erro durante execução do main.py:")
     print(e)
