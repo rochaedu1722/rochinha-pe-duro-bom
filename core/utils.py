5db31a41a6f259003820255bafa7c56c90c12872
rochaedu1722 <rochaedu@icloud.com>
2025-06-13 22:07:37 -0300

    Delete core directory

--- core/utils.py
+++
@@ -1,9 +0,0 @@
-# Funções auxiliares e cálculo de fair odds
-
-def calcular_kelly(probabilidade: float, odd: float, fracao: float = 1.0) -> float:
-    """
-    Calcula a fração de stake com base no critério de Kelly.
-    """
-    valor = (probabilidade * (odd - 1) - (1 - probabilidade)) / (odd - 1)
-    valor_kelly = max(valor, 0)
-    return round(valor_kelly * fracao, 4)
